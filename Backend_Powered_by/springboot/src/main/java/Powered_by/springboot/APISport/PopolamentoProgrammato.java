11476b5bf6e7f743bcb3df5aae1295d6e56d6c81
EdwardCaon <126809042+EdwardCaon@users.noreply.github.com>
2024-01-24 00:06:29 +0100

    Delete Backend_Powered_by directory

--- Backend_Powered_by/springboot/src/main/java/Powered_by/springboot/APISport/PopolamentoProgrammato.java
+++
@@ -1,8 +0,0 @@
-package Powered_by.springboot.APISport;
-
-public class PopolamentoProgrammato {
-    //chimata giornaliera per aggiornare i game
-    //chimata giornaliera per aggiornare gli score
-    //chiamata per le statistiche delle partitee giornaliere dei team
-    //chiamata per modificare le statistiche stagionali dei team che hanno giocato
-}
